9a40d0118cc562f929871ee43efe494413500f1d
tolliverdanielle <tolliver.da@northeastern.edu>
2021-08-04 19:14:39 -0400

    game plan and links for reference

--- app/src/main/java/edu/neu/madcourse/timber/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/timber/MainActivity.java
@@ -10,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
+
+        // Danielle TODO
+        // TODO: Homescreen design
+        // TODO: Longitude and latitude calculation (20 mi) - https://gis.stackexchange.com/questions/5821/calculating-latitude-longitude-x-miles-from-point
+        // TODO bottom menu: https://github.com/PhanVanLinh/AndroidBottomMenu
     }
 }
