1c93919d0ba57e55047a7c0b7a741870127db57f
lukaskatko <lukaskatko@gmail.com>
2021-08-05 22:35:06 -0400

    swipe test works now

--- app/src/main/java/edu/neu/madcourse/timber/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/timber/MainActivity.java
@@ -2,13 +2,17 @@ package edu.neu.madcourse.timber;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+        Intent startButtonActivity = new Intent(getApplicationContext(), SwipeTestActivity.class);
+        startActivity(startButtonActivity);
+        setContentView(R.layout.swipe_test);
     }
 }
