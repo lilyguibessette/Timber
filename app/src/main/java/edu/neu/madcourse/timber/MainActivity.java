bb84008683e5c881a515dd7dc5b6f9e5fe3cfb49
tolliverdanielle <tolliver.da@northeastern.edu>
2021-08-07 18:11:53 -0400

    push

--- app/src/main/java/edu/neu/madcourse/timber/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/timber/MainActivity.java
@@ -47,7 +47,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
-
         startActivity(new Intent(MainActivity.this, HomepageActivity.class));
 
 
