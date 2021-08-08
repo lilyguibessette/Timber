8fef8cc854c130171791047f4c7558ad185e0303
lukaskatko <lukaskatko@gmail.com>
2021-08-07 23:49:16 -0400

    trying to make images work

--- app/src/main/java/edu/neu/madcourse/timber/NewsFeedAdapter.java
+++ app/src/main/java/edu/neu/madcourse/timber/NewsFeedAdapter.java
@@ -1,15 +1,29 @@
 package edu.neu.madcourse.timber;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.ByteArrayInputStream;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 
+import static android.content.ContentValues.TAG;
+
 public class NewsFeedAdapter extends RecyclerView.Adapter<NewsFeedHolder>{
+
+    View view;
     private final ArrayList<NewsFeedPost> newsFeedHistory;
 
     public NewsFeedAdapter(ArrayList<NewsFeedPost> newsFeedHistory) {
@@ -18,7 +32,7 @@ public class NewsFeedAdapter extends RecyclerView.Adapter<NewsFeedHolder>{
 
     @Override
     public NewsFeedHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_post, parent, false);
+        view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_post, parent, false);
         return new NewsFeedHolder(view);
     }
 
@@ -28,6 +42,42 @@ public class NewsFeedAdapter extends RecyclerView.Adapter<NewsFeedHolder>{
         if (currentItem != null) {
             Log.e("onBindViewHolder", currentItem.toString());
             holder.post_username.setText(currentItem.getUsername());
+
+            // Reference to an image file in Cloud Storage
+            StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+            StorageReference cutePuppyRef = storageReference.child("cute puppy.jpg");
+            StorageReference newImageRef = storageReference.child("testImage");
+
+            Bitmap testBM = BitmapFactory.decodeResource(view.getResources(),
+                    R.drawable.sample1);
+            // Your Bitmap.
+
+            int byteSize = testBM.getRowBytes() * testBM.getHeight();
+            ByteBuffer byteBuffer = ByteBuffer.allocate(byteSize);
+            testBM.copyPixelsToBuffer(byteBuffer);
+
+            // Get the byteArray.
+            byte[] byteArray = byteBuffer.array();
+
+
+            Log.e(TAG,"putBytes start");
+
+            newImageRef.putBytes(byteArray);
+
+            Log.e(TAG,"putBytes end");
+
+
+            // ImageView in your Activity
+            ImageView imageView = view.findViewById(R.id.post_image);
+
+
+            // Download directly from StorageReference using Glide
+            // (See MyAppGlideModule for Loader registration)
+            Glide.with(view)
+                    .asDrawable()
+                    .load(newImageRef)
+                    .into(imageView);
+
             holder.post_image_id.setImageResource(currentItem.getPost_id());
             holder.post_description.setText(currentItem.getDescription());
         }
