02c2b1c3d88ebc5b4deb06c80633d4f48457bb8c
tolliverdanielle <tolliver.da@northeastern.edu>
2021-08-08 20:48:57 -0400

    made some changes to the styles/themes

--- app/src/main/java/edu/neu/madcourse/timber/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/timber/MainActivity.java
@@ -90,6 +90,10 @@ public class MainActivity extends AppCompatActivity implements CreateUserDialogF
             startActivity(new Intent(MainActivity.this, HomepageActivity.class));
         }
         setContentView(R.layout.login_screen);
+
+        // hide the action bar for aesthetics
+        getSupportActionBar().hide();
+
         login_button = findViewById(R.id.login_button);
         FirebaseMessaging.getInstance().getToken().addOnCompleteListener(task -> {
 
@@ -248,15 +252,6 @@ public class MainActivity extends AppCompatActivity implements CreateUserDialogF
         }).start();
     }
 
-/*
-
-
-        // hide the action bar for aesthetics
-        getSupportActionBar().hide();
-
-
-    }
-*/
 
     private Location getLocation(){
 
